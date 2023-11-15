141798ba77be771bb7ae1dffc4aac1f2578b9f2c
heleenbrueggen <heleen355@gmail.com>
2023-11-15 11:11:40 +0100

    Update Multilevel BART.R
    Adding model fits for multiple packages using BART

--- R/Multilevel BART.R
+++ R/Multilevel BART.R
@@ -9,11 +9,24 @@ library(BART)
 library(lme4)
 library(dbarts)
 
-
-(fm1 <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy))
-summary(fm1)
-stan4bart(y ~ 1 + x1 + z + x1*z + (x1|group), data = simdata[[1]])
-
-
-fit <- rbart_vi(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2, group.by = group, data = simdata[[1]])
-summary(fit)
+################
+# lme4 package #
+################
+lme4_fit <- lmer(y ~ 1 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2 + x1*z1 + x2*z1 + x3*z2 + (x1|group) + (x2|group) + (x3|group), REML = FALSE, data = simdata[[1]])
+summary(lme4_fit)
+#####################
+# stan4bart package #
+#####################
+stan4bart(y ~ bart(z1*(x1 + x2) + x3*z2 + x4 + x5 + x6 + x7) + (x1|group) + (x2|group) + (x3|group), data = simdata[[1]])
+##################
+# dbarts package #
+##################
+dbarts_fit <- rbart_vi(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2, group.by = group, data = simdata[[1]])
+summary(dbarts_fit)
+dbarts_fit$yhat.train.mean
+dbarts_fit$ranef.mean
+################
+# BART package #
+################
+BART_fit <- wbart(x.train = simdata[[1]][,3:11] %>% as.matrix(), y.train = simdata[[1]]$y)
+BART_fit$yhat.train.mean # train data fits
