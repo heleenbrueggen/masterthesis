b4b999faeac49575f74eebffa170e8bbbac8fde2
heleenbrueggen <heleen355@gmail.com>
2023-11-19 13:06:48 +0100

    saving the simulation code

--- R/Rommel Simulaties.R
+++ R/Rommel Simulaties.R
@@ -2,6 +2,67 @@
 # Rommel #
 ##########
 #####
+# Working Simulation with ICC in u0
+#####
+replicate(n = 1000,
+          expr = tibble(
+            # individual id
+            id = 1:(ngroup * groupsize),
+            # group id
+            group = rep(1:ngroup, each = groupsize),
+            # residual variance
+            eij = rnorm(n = ngroup * groupsize, mean = 0, sd = 5),
+            # level 1 variables
+            x1 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2.5),
+            x2 = rnorm(n = ngroup * groupsize, mean = 0, sd = 3),
+            x3 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2),
+            x4 = rnorm(n = ngroup * groupsize, mean = 0, sd = 3.4),
+            x5 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2),
+            x6 = rnorm(n = ngroup * groupsize, mean = 0, sd = 1.5),
+            x7 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2.3),
+            # level 2 variables
+            z1 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            z2 = rep(rnorm(n = ngroup, mean = 0, sd = 1.6), each = groupsize),
+            # random slopes
+            u6 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            u5 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            u4 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            u3 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            u2 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            u1 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+            u0 = rep(rnorm(n = ngroup,
+                           mean = 0,
+                           sd = uniroot(function(varu0, g00, g01, g11, g20, g21, g30, g32, g40, g50, g60, g70, z1, z2, x1, x2, x3, x4, x5, x6, x7, eij, icc) {
+
+                             daticc <- (icc - ((var(g00 + g01 * z1) + varu0) / (var(g00 + g01 * z1) + varu0 +
+                                                                                  var((g10 + g11 * z1 + u1)*x1) +
+                                                                                  var((g20 + g21 * z1 + u2)*x2) +
+                                                                                  var((g30 + g32 * z2 + u3)*x3) +
+                                                                                  var((g40 + u4)*x4) +
+                                                                                  var((g50 + u5)*x5) +
+                                                                                  var((g60 + u6)*x6) +
+                                                                                  var(g70*x7) + var(eij))))
+
+                             return(daticc)
+                           }, interval = c(0, 100),
+                           tol = .0001,
+                           extendInt = 'yes',
+                           maxiter = 1000, g00=g00, g01=g01, g11=g11, g20=g20, g21=g21, g30=g30, g32=g32, g40=g40, g50=g50, g60=g60, g70=g70, z1=z1, z2=z2, x1=x1, x2=x2, x3=x3, x4=x4, x5=x5, x6=x6, x7=x7, eij=eij, icc=icc)$root %>% as.numeric() %>% sqrt()), each = groupsize),
+            # coefficient generation (including random slopes and cross-level interactions)
+            beta0j = g00 + g01 * z1 + u0,
+            beta1j = g10 + g11 * z1 + u1,
+            beta2j = g20 + g21 * z1 + u2,
+            beta3j = g30 + g32 * z2 + u3,
+            beta4j = g40 + u4,
+            beta5j = g50 + u5,
+            beta6j = g60 + u6,
+            beta7j = g70,
+            # generation of dependent variable y
+            y = beta0j + beta1j * x1 + beta2j * x2 + beta3j * x3 + beta4j * x4 + beta5j * x5 + beta6j * x6 * beta7j * x7 + eij) %>%
+            # taking out terms that are only used for model generation
+            select(-u0, -u1, -u2, -u3, -u4, -u5, -u6, -eij, -beta0j, -beta1j, -beta2j, -beta3j, -beta4j, -beta5j, -beta6j, -beta7j),
+          simplify = FALSE)
+#####
 # Multivariate data simulation
 #####
 simdata <- replicate(n = 1000,
