e7fbbf92690afa526a830c094d6ef9fa9b7ad172
heleenbrueggen <heleen355@gmail.com>
2023-11-27 10:18:46 +0100

    Update Multilevel BART.R

--- R/Multilevel BART.R
+++ R/Multilevel BART.R
@@ -6,7 +6,6 @@
 #############
 library(stan4bart)
 library(BART)
-library(lme4)
 library(dbarts)
 library(bartMachine)
 library(bartMan)
@@ -16,15 +15,10 @@ library(ggplot2)
 # Setting seed #
 ################
 set.seed(123)
-################
-# lme4 package #
-################
-lme4_fit <- lmer(y ~ 1 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2 + x1*z1 + x2*z1 + x3*z2 + (x1|group) + (x2|group) + (x3|group), REML = FALSE, data = simdata[[1]], start = list(theta = matrix(c(rep(0, 9)), ncol = 3)))
-summary(lme4_fit)
 #####################
 # stan4bart package #
 #####################
-stan4bart(y ~ bart(z1*(x1 + x2) + x3*z2 + x4 + x5 + x6 + x7) + (x1|group) + (x2|group) + (x3|group), data = simdata[[1]])
+stand4bart_fit <- stan4bart(y ~ bart(z1*(x1 + x2) + x3*z2 + x4 + x5 + x6 + x7) + (x1|group) + (x2|group) + (x3|group), data = simdatasets$simdata_ngroup_30_groupsize_5_icc_0.5_mar_mcar_0_g_0.2[[1]])
 ##################
 # dbarts package #
 ##################
@@ -33,20 +27,19 @@ dbarts_fit <- rbart_vi(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2,
                        data = simdatasets$simdata_ngroup_30_groupsize_5_icc_0.5_mar_mcar_0_g_0.2[[1]],
                        keepTrees = TRUE,
                        verbose = FALSE)
-dbarts_trees <- extract(dbarts_fit, 'trees')
-print(dbarts_trees) %>% tail()
 
-dbarts_fit <- bart(x.train = simdata[[1]][,3:11] %>% as.matrix(),
-                   y.train = simdata[[1]]$y,
-                   ntree = 50,
-                   keeptrees = TRUE,
-                   nskip = 100,
-                   ndpost = 1000)
+# plotting y against predicted y from dbarts
+pred <- tibble(y = simdatasets$simdata_ngroup_30_groupsize_5_icc_0.5_mar_mcar_0_g_0.2[[1]]$y,
+               ypred = fitted(dbarts_fit, type = 'ev', sample = 'train'))
 
-summary(dbarts_fit)
-dbarts_fit$yhat.train.mean
-dbarts_fit$ranef.mean
-
+ggplot(data = pred, mapping = aes(
+  x = y,
+  y = ypred
+)) +
+  geom_point(color = 'royalblue', size = .5) +
+  theme_minimal() +
+  geom_abline(slope = 1, intercept = 0, color = 'royalblue4') +
+  geom_rug(color = 'royalblue', linewidth = .1)
 ################
 # BART package #
 ################
@@ -68,7 +61,7 @@ BART_trees$tier <- as.integer(floor(log2(BART_trees$node)))
 
 
 # plotting y against predicted y from BART
-pred <- tibble(y = simdata[[1]]$y,
+pred <- tibble(y = simdatasets$simdata_ngroup_30_groupsize_5_icc_0.5_mar_mcar_0_g_0.2[[1]]$y,
                ypred = BART_fit$yhat.train.mean)
 
 ggplot(data = pred, mapping = aes(
