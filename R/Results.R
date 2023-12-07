999377b3b18fdf1998128ef1efef68f017a643cd
heleenbrueggen <heleen355@gmail.com>
2023-12-07 15:21:24 +0100

    Results folder

---
+++ R/Results.R
@@ -0,0 +1,26 @@
+###########
+# Results #
+###########
+#############
+# Libraries #
+#############
+
+################
+# Setting seed #
+################
+set.seed(123)
+########
+# Data #
+########
+load("~/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/data/simdatasets.RData")
+##########
+# Models #
+##########
+load('/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/results/stan4bart_fits.RData')
+load('/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/results/rbart_evs.RData')
+load('/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/results/rbart_ranefs.RData')
+load('/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/results/bart_fits.RData')
+load('/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/results/gbart_fits.RData')
+###########
+# Results #
+###########
