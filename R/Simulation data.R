0d16bdf0d289ae0f49dae2f960e174ee3835a891
heleenbrueggen <heleen355@gmail.com>
2024-01-19 13:31:32 +0100

    Simulating 1000 data sets

--- R/Simulation data.R
+++ R/Simulation data.R
@@ -64,83 +64,125 @@ combinations <- expand.grid(
 # Simulating data #
 ###################
 simdatasets <- list()
-for (i in 1:nrow(combinations)) {
-
+for (i in seq_len(nrow(combinations))) {
   ngroup <- combinations$ngroup[i]
   groupsize <- combinations$groupsize[i]
   icc <- combinations$icc[i]
   # Overall intercept
-  g00 = 10
+  g00 <- 10
   # Individual effects
-  g10 = combinations$g[i]
-  g20 = combinations$g[i]
-  g30 = combinations$g[i]
-  g40 = combinations$g[i]
-  g50 = combinations$g[i]
-  g60 = combinations$g[i]
-  g70 = combinations$g[i]
-  if (icc != 0)
-  {g01 = .5; g02 = .5; g11 = .35; g21 = .35; g32 = .35}
-  else
-  {g01 = 0; g02 = 0; g11 = 0; g21 = 0; g32 = 0}
+  g10 <- combinations$g[i]
+  g20 <- combinations$g[i]
+  g30 <- combinations$g[i]
+  g40 <- combinations$g[i]
+  g50 <- combinations$g[i]
+  g60 <- combinations$g[i]
+  g70 <- combinations$g[i]
+  if (icc != 0) {
+    g01 <- .5
+    g02 <- .5
+    g11 <- .35
+    g21 <- .35
+    g32 <- .35
+  } else {
+    g01 <- 0
+    g02 <- 0
+    g11 <- 0
+    g21 <- 0
+    g32 <- 0
+  }
 
-  simdata <- replicate(n = 1000,
-                       expr = tibble(
-                         # individual id
-                         id = 1:(ngroup * groupsize),
-                         # group id
-                         group = rep(1:ngroup, each = groupsize),
-                         # residual variance
-                         eij = rnorm(n = ngroup * groupsize, mean = 0, sd = 5),
-                         # level 1 variables
-                         x1 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2.5),
-                         x2 = rnorm(n = ngroup * groupsize, mean = 0, sd = 3),
-                         x3 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2),
-                         x4 = rnorm(n = ngroup * groupsize, mean = 0, sd = 3.4),
-                         x5 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2),
-                         x6 = rnorm(n = ngroup * groupsize, mean = 0, sd = 1.5),
-                         x7 = rnorm(n = ngroup * groupsize, mean = 0, sd = 4.4),
-                         # level 2 variables
-                         z1 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
-                         z2 = rep(rnorm(n = ngroup, mean = 0, sd = 1.6), each = groupsize),
-                         # random slopes
-                         u6 = if (icc != 0) {rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         u5 = if (icc != 0) {rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         u4 = if (icc != 0) {rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         u3 = if (icc != 0) {rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         u2 = if (icc != 0) {rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         u1 = if (icc != 0) {rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         u0 = if (icc != 0) {rep(rnorm(n = ngroup,
-                                                       mean = 0,
-                                                       sd = uniroot(var.u0,
-                                                                    interval = c(0, 100),
-                                                                    tol = .000001,
-                                                                    extendInt = 'yes',
-                                                                    maxiter = 1000,
-                                                                    g00=g00,
-                                                                    g01=g01, g02= g02, g11=g11, g21=g21, g32=g32,
-                                                                    g10=g10, g20=g20, g30=g30, g40=g40, g50=g50, g60=g60, g70=g70,
-                                                                    z1=z1, z2=z2,
-                                                                    x1=x1, x2=x2, x3=x3, x4=x4, x5=x5, x6=x6, x7=x7,
-                                                                    u1=u1, u2=u2, u3=u3, u4=u4, u5=u5, u6=u6,
-                                                                    eij=eij,
-                                                                    icc=icc)$root %>% sqrt()), each = groupsize)} else {rep(0, (ngroup * groupsize))},
-                         # coefficient generation (including random slopes and cross-level interactions)
-                         beta0j = g00 + g01 * z1 + g02 * z2 + u0,
-                         beta1j = g10 + g11 * z1 + u1,
-                         beta2j = g20 + g21 * z1 + u2,
-                         beta3j = g30 + g32 * z2 + u3,
-                         beta4j = g40 + u4,
-                         beta5j = g50 + u5,
-                         beta6j = g60 + u6,
-                         beta7j = g70,
-                         # generation of dependent variable y
-                         y = beta0j + beta1j * x1 + beta2j * x2 + beta3j * x3 + beta4j * x4 + beta5j * x5 + beta6j * x6 * beta7j * x7 + eij) %>%
-                         # taking out terms that are only used for model generation
-                         select(-u0, -u1, -u2, -u3, -u4, -u5, -u6, -eij, -beta0j, -beta1j, -beta2j, -beta3j, -beta4j, -beta5j, -beta6j, -beta7j),
-                       simplify = FALSE)
+  simdata <- replicate(
+    n = 1000,
+    expr = tibble(
+      # individual id
+      id = 1:(ngroup * groupsize),
+      # group id
+      group = rep(1:ngroup, each = groupsize),
+      # residual variance
+      eij = rnorm(n = ngroup * groupsize, mean = 0, sd = 5),
+      # level 1 variables
+      x1 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2.5),
+      x2 = rnorm(n = ngroup * groupsize, mean = 0, sd = 3),
+      x3 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2),
+      x4 = rnorm(n = ngroup * groupsize, mean = 0, sd = 3.4),
+      x5 = rnorm(n = ngroup * groupsize, mean = 0, sd = 2),
+      x6 = rnorm(n = ngroup * groupsize, mean = 0, sd = 1.5),
+      x7 = rnorm(n = ngroup * groupsize, mean = 0, sd = 4.4),
+      # level 2 variables
+      z1 = rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize),
+      z2 = rep(rnorm(n = ngroup, mean = 0, sd = 1.6), each = groupsize),
+      # random slopes
+      u6 = if (icc != 0) {
+        rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      u5 = if (icc != 0) {
+        rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      u4 = if (icc != 0) {
+        rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      u3 = if (icc != 0) {
+        rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      u2 = if (icc != 0) {
+        rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      u1 = if (icc != 0) {
+        rep(rnorm(n = ngroup, mean = 0, sd = 1), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      u0 = if (icc != 0) {
+        rep(rnorm(
+          n = ngroup,
+          mean = 0,
+          sd = uniroot(var.u0,
+            interval = c(0, 100),
+            tol = .000001,
+            extendInt = "yes",
+            maxiter = 1000,
+            g00 = g00,
+            g01 = g01, g02 = g02, g11 = g11, g21 = g21, g32 = g32,
+            g10 = g10, g20 = g20, g30 = g30, g40 = g40, g50 = g50, g60 = g60, g70 = g70,
+            z1 = z1, z2 = z2,
+            x1 = x1, x2 = x2, x3 = x3, x4 = x4, x5 = x5, x6 = x6, x7 = x7,
+            u1 = u1, u2 = u2, u3 = u3, u4 = u4, u5 = u5, u6 = u6,
+            eij = eij,
+            icc = icc
+          )$root %>% sqrt()
+        ), each = groupsize)
+      } else {
+        rep(0, (ngroup * groupsize))
+      },
+      # coefficient generation (including random slopes and cross-level interactions)
+      beta0j = g00 + g01 * z1 + g02 * z2 + u0,
+      beta1j = g10 + g11 * z1 + u1,
+      beta2j = g20 + g21 * z1 + u2,
+      beta3j = g30 + g32 * z2 + u3,
+      beta4j = g40 + u4,
+      beta5j = g50 + u5,
+      beta6j = g60 + u6,
+      beta7j = g70,
+      # generation of dependent variable y
+      y = beta0j + beta1j * x1 + beta2j * x2 + beta3j * x3 + beta4j * x4 + beta5j * x5 + beta6j * x6 * beta7j * x7 + eij
+    ) %>%
+      # taking out terms that are only used for model generation
+      select(-u0, -u1, -u2, -u3, -u4, -u5, -u6, -eij, -beta0j, -beta1j, -beta2j, -beta3j, -beta4j, -beta5j, -beta6j, -beta7j),
+    simplify = FALSE
+  )
 
-  name <- paste('simdata', colnames(combinations)[1], combinations[i,1], colnames(combinations)[2], combinations[i,2], colnames(combinations)[3], combinations[i,3], colnames(combinations)[4], combinations[i,4], colnames(combinations)[5], combinations[i,5], sep = '_')
+  name <- paste("simdata", colnames(combinations)[1], combinations[i, 1], colnames(combinations)[2], combinations[i, 2], colnames(combinations)[3], combinations[i, 3], colnames(combinations)[4], combinations[i, 4], colnames(combinations)[5], combinations[i, 5], colnames(combinations)[6], combinations[i, 6], sep = "_")
   simdatasets[[name]] <- simdata
 }
 ####################
@@ -152,37 +194,24 @@ for (i in 1:nrow(combinations)) {
 # Checking population generation model over all simulated data sets
 simdata %>%
   map(~.x %$%
-        lmer(y ~ 1 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2 + x1*z1 + x2*z1 + x3*z2 + (x1|group) + (x2|group) + (x3|group), REML = FALSE) %>%
+        lmer(y ~ 1 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2 + x1 * z1 + x2 * z1 + x3 * z2 + (x1 | group) + (x2 | group) + (x3 | group), REML = FALSE) %>%
         summary %>%
         coefficients %>%
-        .[,1]) %>%
+        .[, 1]) %>%
   Reduce("+", .) / length(simdata)
 ################
 # Checking ICC #
 ################
-# # Generating function for ICC calculation
-# iccfunction <- function (data) {
-#   icc <- var(data$beta0j)/var(data$y)
-#   return(icc)
-# } # Needs beta0j to be in data set
-# # Checking ICC over all simulated data sets
-# iccvalues <- rep(0, 288)
-# for(i in 1:288) {
-#   iccvalues[i] <- simdatasets[[i]] %>%
-#     map(~.x %$%
-#           iccfunction(.)) %>%
-#     Reduce("+", .)/length(simdatasets[[i]])
-# }
-
 # Checking ICC values of all datasets
 iccvalues <- rep(0, 288)
 for (i in 1:288) {
   iccvalues[i] <- simdatasets[[i]] %>%
-    map(~.x %$%
-          lmer(y ~ 1 + (1|group), REML = FALSE) %>%
-          summ %>%
-          .$gvars %>%
-          .[3]) %>% as.numeric() %>%
+    map(~ .x %$%
+      lmer(y ~ 1 + (1 | group), REML = FALSE) %>%
+      summ() %>%
+      .$gvars %>%
+      .[3]) %>%
+    as.numeric() %>%
     Reduce("+", .) / length(simdatasets[[i]])
 }
 # Check if they are the same as specified
@@ -190,7 +219,7 @@ cbind(iccvalues %>% round(digits = 3), combinations) %>% tail()
 ###############
 # Saving data #
 ###############
-save(simdatasets, file = '/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/data/simdatasets.RData')
+save(simdatasets, file = "data/simdatasets.RData")
 ############
 # Appendix #
 ############
