251444c717fb0fb4cf7854cfa807a38365fb08b5
heleenbrueggen <heleen355@gmail.com>
2024-02-07 16:47:11 +0100

    Adding libraries and code for imputation functions

--- R/Imputation BART.R
+++ R/Imputation BART.R
@@ -5,8 +5,11 @@
 # Libraries #
 #############
 library(devtools)
-install_github("heleenbrueggen/mice@impute.mbart")
+# install_github("heleenbrueggen/mice@impute.mbart", force = TRUE)
 library(mice)
+library(furrr)
+library(magrittr)
+library(readr)
 ################
 # Setting seed #
 ################
@@ -43,4 +46,90 @@ for (i in seq_len(nrow(combinations))) {
     sep = "_"
   )
   simdatasets_miss[[i]] <- read_rds(paste("data/missing/", name, ".rds", sep = ""))
+}
+#############################
+# Storing names of datasets #
+#############################
+names <- rep(NA, 576)
+for (i in seq_len(nrow(combinations))) {
+  names[i] <- paste("simdata",
+    colnames(combinations)[1], combinations[i, 1],
+    colnames(combinations)[2], combinations[i, 2],
+    colnames(combinations)[3], combinations[i, 3],
+    colnames(combinations)[4], combinations[i, 4],
+    colnames(combinations)[5], combinations[i, 5],
+    colnames(combinations)[6], combinations[i, 6],
+    sep = "_"
+  )
+}
+##############
+# Imputation #
+##############
+########
+# bart #
+########
+imputed_bart <- list()
+for (i in seq_len(nrow(combinations))) {
+    # Logging iteration
+    cat("Processing iteration:", i, "\n")
+    if (combinations[i, "miss"] != 0) {
+        imputed_bart[[i]] <- simdatasets_miss[[i]] %>%
+            future_map(function(x) {
+                mice(x,
+                    method = "bart",
+                    m = 5,
+                    maxit = 10,
+                    seed = 123
+                ) |> complete()
+            }, .options = furrr_options(seed = 123))
+    } else {
+        imputed_bart[[i]] <- simdatasets_miss[[i]]
+    }
+}
+#############
+# stan4bart #
+#############
+imputed_stan4bart <- list()
+for (i in seq_len(nrow(combinations))) {
+    # Logging iteration
+    cat("Processing iteration:", i, "\n")
+    if (combinations[i, "miss"] != 0) {
+        imputed_stan4bart[[i]] <- simdatasets_miss[[i]] %>%
+            future_map(function(x) {
+                mice(x,
+                    method = "2l.bart",
+                    m = 5,
+                    maxit = 10,
+                    seed = 123
+                ) |> complete()
+            }, .options = furrr_options(seed = 123))
+    } else {
+        imputed_stan4bart[[i]] <- simdatasets_miss[[i]]
+    }
+}
+#########
+# rbart #
+#########
+imputed_rbart <- list()
+for (i in seq_len(nrow(combinations))) {
+    # Logging iteration
+    cat("Processing iteration:", i, "\n")
+    if (combinations[i, "miss"] != 0) {
+        imputed_rbart[[i]] <- simdatasets_miss[[i]] %>%
+            future_map(function(x) {
+                pred <- make.predictorMatrix(x)
+                pred[, "group"] <- -2
+                pred["group", "group"] <- 0
+                pred[, "id"] <- 0
+                mice(x,
+                    method = "2l.rbart",
+                    pred = pred,
+                    m = 5,
+                    maxit = 10,
+                    seed = 123
+                ) |> complete()
+            }, .options = furrr_options(seed = 123))
+    } else {
+        imputed_rbart[[i]] <- simdatasets_miss[[i]]
+    }
 }
