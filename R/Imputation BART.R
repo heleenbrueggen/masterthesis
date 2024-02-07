5156129b0a2001bd2ef92592b4022871ac1c247d
heleenbrueggen <heleen355@gmail.com>
2024-02-07 14:15:09 +0100

    Adding code for libraries, seed and loading data for imputation file

--- R/Imputation BART.R
+++ R/Imputation BART.R
@@ -1,4 +1,46 @@
 ###################
 # Imputation BART #
 ###################
-
+#############
+# Libraries #
+#############
+library(devtools)
+install_github("heleenbrueggen/mice@impute.mbart")
+library(mice)
+################
+# Setting seed #
+################
+set.seed(123)
+#######################
+# Defining parameters #
+#######################
+ngroups <- c(30, 50)
+groupsizes <- c(5, 15, 35, 50)
+iccs <- c(0, .05, .3, .5)
+mar_mcar <- c("mar", "mcar")
+miss <- c(0, 25, 50)
+g <- c(.2, .5, .8)
+combinations <- expand.grid(
+  ngroup = ngroups,
+  groupsize = groupsizes,
+  icc = iccs,
+  mar_mcar = mar_mcar,
+  miss = miss,
+  g = g
+)
+#############
+# Load data #
+#############
+simdatasets_miss <- list()
+for (i in seq_len(nrow(combinations))) {
+  name <- paste("simdata_miss",
+    colnames(combinations)[1], combinations[i, 1],
+    colnames(combinations)[2], combinations[i, 2],
+    colnames(combinations)[3], combinations[i, 3],
+    colnames(combinations)[4], combinations[i, 4],
+    colnames(combinations)[5], combinations[i, 5],
+    colnames(combinations)[6], combinations[i, 6],
+    sep = "_"
+  )
+  simdatasets_miss[[i]] <- read_rds(paste("data/missing/", name, ".rds", sep = ""))
+}
