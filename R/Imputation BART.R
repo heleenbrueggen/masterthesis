2fe7959d5f7af6332026e4448eab65e0a9fb2f5c
heleenbrueggen <heleen355@gmail.com>
2024-03-06 11:42:14 +0100

    Adding code for pmm and multilevel pmm imputation

--- R/Imputation BART.R
+++ R/Imputation BART.R
@@ -7,9 +7,11 @@
 library(devtools)
 # install_github("heleenbrueggen/mice@impute.mbart")
 library(mice)
+library(miceadds)
 library(furrr)
 library(magrittr)
 library(readr)
+library(lme4)
 ################
 # Setting seed #
 ################
@@ -62,9 +64,81 @@ for (i in seq_len(nrow(combinations))) {
     sep = "_"
   )
 }
+pred <- make.predictorMatrix(simdatasets_miss[[65]][[1]])
+pred[, "id"] <- 0
+imp <- mice(simdatasets_miss[[66]][[1]], method = "pmm", m = 5, maxit = 10, seed = 123)
+fit <- with(imp, lmer(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2 + x1 * z1 + x2 * z1 + x3 * z2 + (1 + x1 + x2| group), REML = FALSE))
+summary(lmer_pool(fit))
+testEstimates(as.mitml.result(fit), var.comp = TRUE)$var.comp
+# Package mltml is not available for my version of R (testEstimates is a function from that package)
 ##############
 # Imputation #
 ##############
+#######
+# pmm #
+#######
+imputed_pmm <- list()
+for (i in seq_len(nrow(combinations))) {
+    # Logging iteration
+    cat("Processing iteration:", i, "\n")
+    if (combinations[i, "miss"] != 0) {
+        imputed_pmm[[i]] <- simdatasets_miss[[i]] %>%
+            future_map(function(x) {
+                pred <- make.predictorMatrix(x)
+                pred[, "id"] <- 0
+                imp <- mice(x,
+                    method = "pmm",
+                    pred = pred,
+                    m = 5,
+                    maxit = 10,
+                    seed = 123
+                ) #|> complete()
+                fit <-  with(imp, lmer(y ~ 1 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + z1 + z2 + x1 * z1 + x2 * z1 + x3 * z2 + (1 + x1 + x2 + x3 + x4 + x5 + x6| group), REML = FALSE)) 
+                summary(pool(fit))
+                testEstimates(as.mitml.result(fit), var.comp = TRUE)$var.comp
+            }, .options = furrr_options(seed = 123))
+    } else {
+        imputed_pmm[[i]] <- simdatasets_miss[[i]]
+    }
+}
+##################
+# multilevel pmm #
+##################
+imputed_2l.pmm <- list()
+for (i in seq_len(nrow(combinations))) {
+    # Logging iteration
+    cat("Processing iteration:", i, "\n")
+    if (combinations[i, "miss"] != 0) {
+        imputed_2l.pmm[[i]] <- simdatasets_miss[[i]] %>%
+            future_map(function(x) {
+                pred <- make.predictorMatrix(x)
+                # pred[, "group"] <- -2
+                pred[, "id"] <- 0
+                pred["id", "id"] <- 0
+                pred["x1",] <- c(0, -2, 0, 2, 2, 2, 2, 2, 1, 1, 1, 1)
+                pred["x2",] <- c(0, -2, 2, 0, 2, 2, 2, 2, 1, 1, 1, 1)
+                pred["x3",] <- c(0, -2, 2, 2, 0, 2, 2, 2, 1, 1, 1, 1)
+                pred["x4",] <- c(0, -2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1)
+                pred["x5",] <- c(0, -2, 2, 2, 2, 2, 0, 2, 1, 1, 1, 1)
+                pred["x6",] <- c(0, -2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 1)
+                pred["x7",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1)
+                pred["z1",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 1, 0, 1, 1)
+                pred["z2",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 1, 1, 0, 1)
+                pred["y",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0)
+                meth <- make.method(x)
+                meth[c("x1", "x2", "x3", "x4", "x5", "x6", "x7", "z1", "z2", "y")] <- c("2l.pmm", "2l.pmm", "2l.pmm", "2l.pmm", "2l.pmm", "2l.pmm", "2l.pmm", "2lonly.pmm", "2lonly.pmm", "2l.pmm")
+                mice(x,
+                    method = meth,
+                    pred = pred,
+                    m = 5,
+                    maxit = 10,
+                    seed = 123
+                ) |> complete()
+            }, .options = furrr_options(seed = 123))
+    } else {
+        imputed_2l.pmm[[i]] <- simdatasets_miss[[i]]
+    }
+}
 ########
 # bart #
 ########
@@ -78,7 +152,7 @@ for (i in seq_len(nrow(combinations))) {
                 mice(x,
                     method = "bart",
                     m = 5,
-                    maxit = 10,
+                    maxit = 5,
                     seed = 123
                 ) |> complete()
             }, .options = furrr_options(seed = 123))
@@ -86,27 +160,8 @@ for (i in seq_len(nrow(combinations))) {
         imputed_bart[[i]] <- simdatasets_miss[[i]]
     }
 }
-#############
-# stan4bart #
-#############
-imputed_stan4bart <- list()
-for (i in seq_len(nrow(combinations))) {
-    # Logging iteration
-    cat("Processing iteration:", i, "\n")
-    if (combinations[i, "miss"] != 0) {
-        imputed_stan4bart[[i]] <- simdatasets_miss[[i]] %>%
-            future_map(function(x) {
-                mice(x,
-                    method = "2l.bart",
-                    m = 5,
-                    maxit = 10,
-                    seed = 123
-                ) |> complete()
-            }, .options = furrr_options(seed = 123))
-    } else {
-        imputed_stan4bart[[i]] <- simdatasets_miss[[i]]
-    }
-}
+write_rds(imputed_bart, file = "data/imputed/imputed_bart.rds")
+imputed_bart <- read_rds("data/imputed/imputed_bart.rds")
 #########
 # rbart #
 #########
@@ -133,6 +188,57 @@ for (i in seq_len(nrow(combinations))) {
         imputed_rbart[[i]] <- simdatasets_miss[[i]]
     }
 }
+write_rds(imputed_rbart, file = "data/imputed/imputed_rbart.rds")
+imputed_rbart <- read_rds("data/imputed/imputed_rbart.rds")
+#############
+# stan4bart #
+#############
+imputed_stan4bart <- list()
+for (i in seq_len(nrow(combinations))) {
+    # Logging iteration
+    cat("Processing iteration:", i, "\n")
+    if (combinations[i, "miss"] != 0) {
+        imputed_stan4bart[[i]] <- simdatasets_miss[[i]] %>%
+            future_map(function(x) {
+                pred <- make.predictorMatrix(x)
+                # pred[, "group"] <- -2
+                pred[, "id"] <- 0
+                pred["id", "id"] <- 0
+                pred["x1",] <- c(0, -2, 0, 2, 2, 2, 2, 2, 1, 1, 1, 1)
+                pred["x2",] <- c(0, -2, 2, 0, 2, 2, 2, 2, 1, 1, 1, 1)
+                pred["x3",] <- c(0, -2, 2, 2, 0, 2, 2, 2, 1, 1, 1, 1)
+                pred["x4",] <- c(0, -2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1)
+                pred["x5",] <- c(0, -2, 2, 2, 2, 2, 0, 2, 1, 1, 1, 1)
+                pred["x6",] <- c(0, -2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 1)
+                pred["x7",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1)
+                pred["z1",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 1, 0, 1, 1)
+                pred["z2",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 1, 1, 0, 1)
+                pred["y",] <- c(0, -2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0)
+                meth <- make.method(x)
+                meth[c("x1", "x2", "x3", "x4", "x5", "x6", "x7", "z1", "z2", "y")] <- "stan4bart"
+                mice(x,
+                    method = meth,
+                    pred = pred,
+                    m = 5,
+                    maxit = 10,
+                    seed = 123
+                ) |> complete()
+            }, .options = furrr_options(seed = 123))
+    } else {
+        imputed_stan4bart[[i]] <- simdatasets_miss[[i]]
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 imputed.bart <- mice(simdatasets_miss[[65]][[1]],
     method = "bart",
     pred = pred,
