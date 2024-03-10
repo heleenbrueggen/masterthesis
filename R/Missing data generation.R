63e0889d6f3151bcc0b868f8e32b6fc2cdb6f398
heleenbrueggen <heleen355@gmail.com>
2024-03-10 19:39:19 +0100

    Updating to not include no missing data sets

--- R/Missing data generation.R
+++ R/Missing data generation.R
@@ -19,11 +19,11 @@ set.seed(123)
 # Defining parameters #
 #######################
 ngroups <- c(30, 50)
-groupsizes <- c(5, 15, 35, 50)
-iccs <- c(0, .05, .3, .5)
+groupsizes <- c(15, 35, 50)
+iccs <- c(0, .3)
 mar_mcar <- c("mar", "mcar")
-miss <- c(0, 25, 50)
-g <- c(.2, .5, .8)
+miss <- c(25, 50)
+g <- c(.2, .5)
 combinations <- expand.grid(
   ngroup = ngroups,
   groupsize = groupsizes,
@@ -32,26 +32,10 @@ combinations <- expand.grid(
   miss = miss,
   g = g
 )
-#############
-# Load data #
-#############
-simdatasets <- list()
-for (i in seq_len(nrow(combinations))) {
-  name <- paste("simdata",
-    colnames(combinations)[1], combinations[i, 1],
-    colnames(combinations)[2], combinations[i, 2],
-    colnames(combinations)[3], combinations[i, 3],
-    colnames(combinations)[4], combinations[i, 4],
-    colnames(combinations)[5], combinations[i, 5],
-    colnames(combinations)[6], combinations[i, 6],
-    sep = "_"
-  )
-  simdatasets[[i]] <- read_rds(paste("data/complete/", name, ".rds", sep = ""))
-}
 #############################
 # Storing names of datasets #
 #############################
-names <- rep(NA, 576)
+names <- rep(NA, nrow(combinations))
 for (i in seq_len(nrow(combinations))) {
   names[i] <- paste("simdata",
     colnames(combinations)[1], combinations[i, 1],
@@ -62,6 +46,13 @@ for (i in seq_len(nrow(combinations))) {
     colnames(combinations)[6], combinations[i, 6],
     sep = "_"
   )
+}
+#############
+# Load data #
+#############
+simdatasets <- list()
+for (i in seq_len(nrow(combinations))) {
+  simdatasets[[i]] <- read_rds(paste("data/complete/", names[i], ".rds", sep = ""))
 }
 ##########################
 # Model based simulation #
@@ -89,71 +80,49 @@ weights[,"x4"] <- weights[,"x4"] * 2
 # test <- ampute(simdatasets[[129]][[1]][,3:12], prop = .03333, patterns = patterns, freq = freq, weights = weights, mech = "MAR", bycases = FALSE)$amp
 
 # Generating missing data
-simdatasets_miss <- list()
 for (i in seq_len(nrow(combinations))) {
   # Logging iteration
   cat("Processing iteration:", i, "\n")
-  if (combinations[i, "miss"] != 0) {
-    if (combinations[i, "mar_mcar"] == "mcar") {
-      simdatasets_miss[[i]] <-
-        simdatasets[[i]] %>%
-        future_map(function(x) {
-          id <- x$id
-          group <- x$group
-          x %>%
-            select(-id, -group) %>%
-            group_by(z1) %>%
-            ampute(
-              prop = (combinations[i, "miss"] * .01),
-              mech = "MCAR",
-              patterns = patterns,
-              freq = freq
-              # bycases = FALSE
-            ) %>%
-            .$amp %>%
-            ungroup() %>%
-            mutate(id = id, group = group)
-        }, .options = furrr_options(seed = 123))
-    } else {
-      simdatasets_miss[[i]] <-
-        simdatasets[[i]] %>%
-        future_map(function(x) {
-          id <- x$id
-          group <- x$group
-          x %>%
-            select(-id, -group) %>%
-            group_by(z1) %>%
-            ampute(
-              prop = (combinations[i, "miss"] * .01),
-              mech = "MAR",
-              type = "RIGHT",
-              patterns = patterns,
-              freq = freq,
-              weights = weights
-              # bycases = FALSE
-            ) %>%
-            .$amp %>% 
-            ungroup() %>%
-            mutate(id = id, group = group)
-        }, .options = furrr_options(seed = 123))
-    }
+  if (combinations[i, "mar_mcar"] == "mcar") {
+    simdatasets_miss <-
+      simdatasets[[i]] %>%
+      future_map(function(x) {
+        others <- x %>% select(-x1, -x2, -x3, -x4, -x5, -x6, -x7, -z1, -z2, -y)
+        x %>%
+          select(x1, x2, x3, x4, x5, x6, z7, z1, z2, y) %>%
+          group_by(z1) %>%
+          ampute(
+            prop = (combinations[i, "miss"] * .01),
+            mech = "MCAR",
+            patterns = patterns,
+            freq = freq
+          ) %>%
+          .$amp %>%
+          ungroup() %>%
+          cbind(., others)
+      }, .options = furrr_options(seed = 123))
   } else {
-    simdatasets_miss[[i]] <- simdatasets[[i]]
+    simdatasets_miss <-
+      simdatasets[[i]] %>%
+      future_map(function(x) {
+        others <- x %>% select(-x1, -x2, -x3, -x4, -x5, -x6, -x7, -z1, -z2, -y)
+        x %>%
+          select(x1, x2, x3, x4, x5, x6, z7, z1, z2, y) %>%
+          group_by(z1) %>%
+          ampute(
+            prop = (combinations[i, "miss"] * .01),
+            mech = "MAR",
+            type = "RIGHT",
+            patterns = patterns,
+            freq = freq,
+            weights = weights
+          ) %>%
+          .$amp %>%
+          ungroup() %>%
+          cbind(., others)
+      }, .options = furrr_options(seed = 123))
   }
-}
-#######################
-# Saving missing data # 
-#######################
-write_rds(simdatasets_miss, file = "data/missing/simdatasets_miss.rds")
-for (i in seq_len(nrow(combinations))) {
-  name <- paste("simdata_miss",
-    colnames(combinations)[1], combinations[i, 1],
-    colnames(combinations)[2], combinations[i, 2],
-    colnames(combinations)[3], combinations[i, 3],
-    colnames(combinations)[4], combinations[i, 4],
-    colnames(combinations)[5], combinations[i, 5],
-    colnames(combinations)[6], combinations[i, 6],
-    sep = "_"
-  )
-  write_rds(simdatasets_miss[[i]], file = paste("data/missing/", name, ".rds", sep = ""))
+
+  # Saving data in appropriate data folder
+  write_rds(simdatasets_miss, file = paste("data/missing/", names[i], ".rds", sep = ""))
 }
