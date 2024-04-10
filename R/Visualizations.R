0697afc0592686482b5debf182ba62887cd0890c
heleenbrueggen <heleen355@gmail.com>
2024-04-10 12:00:05 +0200

    finding the right color palette

--- R/Visualizations.R
+++ R/Visualizations.R
@@ -18,6 +18,9 @@ source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for 
 #########
 # Plots # 
 #########
+cbbPalette <- c("#000000", "#E69F00", "#009E73", "#CC79A7", "#56B4E9", "#D55E00", "#5D478B", "#0072B2", "#F0E442")
+# cPalette <- c("#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0")
+# cPalette <- c("#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7")
 # Bias
 bias1 <- bias_combined %>%
     filter(Term != "eij" & Term != "u0") %>%
@@ -27,7 +30,7 @@ bias1 <- bias_combined %>%
         color = Method
     )) +
     geom_point(position = position_jitter(seed = 123)) +
-    geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123), width = .5) +
+    geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123), width = .3) +
     # geom_pointrange(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123)) +
     # geom_linerange(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123)) +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
@@ -38,7 +41,8 @@ bias1 <- bias_combined %>%
     # scale_color_viridis_d() +
     # scale_color_brewer(palette = "Dark2") +
     # scale_color_manual(values = c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#E6AB02", "#666666")) +
-    scale_color_lancet() +
+    # scale_color_lancet() +
+    scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 8, angle = 45, hjust = 1), legend.position = "bottom") +
     labs(
@@ -55,13 +59,13 @@ bias2 <- bias_combined %>%
         color = Method
     )) +
     geom_point(position = position_jitter(seed = 123)) +
-    geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123), width = .1) +
+    geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123), width = .05) +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
     scale_x_continuous(n.breaks = 15, minor_breaks = seq(-40, 60, 2.5)) +
     geom_vline(xintercept = 0, color = "gray40") +
     # geom_vline(xintercept = .10, linetype = "dashed", color = "gray40") +
     # geom_vline(xintercept = -.10, linetype = "dashed", color = "gray40") +
-    scale_color_lancet() +
+    scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 8, angle = 45, hjust = 1), legend.position = "bottom") +
     labs(
@@ -78,14 +82,14 @@ coverage <- coverage_combined %>%
         color = Method
     )) +
     geom_point(position = position_jitter(seed = 123)) +
-    geom_errorbar(aes(xmin = Coverage - MCSE, xmax = Coverage + MCSE), position = position_jitter(seed = 123), width = .5) +
+    geom_errorbar(aes(xmin = Coverage - MCSE, xmax = Coverage + MCSE), position = position_jitter(seed = 123), width = .3) +
     geom_vline(xintercept = .95, color = "gray40") +
     geom_vline(xintercept = .925, linetype = "dashed", color = "gray40") +
     geom_vline(xintercept = .975, linetype = "dashed", color = "gray40") +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
     # scale_x_continuous(limits = c(.4, 1), breaks = c(.4, .45, .5, .55, .6, .65, .7, .75, .8, .85, .9, .95, 1)) +
     scale_x_continuous(n.breaks = 20) +
-    scale_color_lancet() +
+    scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 8, angle = 45, hjust = 1), legend.position = "bottom") +
     labs(
@@ -107,7 +111,7 @@ ciw <- ciw_combined %>%
     # geom_linerange(aes(xmin = 0, xmax = CIW), position = position_jitter(seed = 123)) +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
     scale_x_continuous(n.breaks = 18) +
-    scale_color_lancet() +
+    scale_color_manual(values = cbbPalette) +
     # scale_fill_lancet() +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 8, angle = 45, hjust = 1), legend.position = "bottom") +
@@ -128,7 +132,7 @@ mse_combined %>%
     )) +
     geom_jitter() +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
-    scale_color_lancet() +
+    scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 7, hjust = 1), legend.position = "bottom") +
     labs(
@@ -146,7 +150,7 @@ mse_combined %>%
     )) +
     geom_jitter() +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
-    scale_color_lancet() +
+    scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 7, hjust = 1), legend.position = "bottom") +
     labs(
