e2fb0e788aeec98065449579e53bceab1e6ea01e
heleenbrueggen <heleen355@gmail.com>
2024-04-19 14:45:16 +0200

    adding separate plot for cross-level interactions, and changing the axis-font-size of some plots

--- R/Visualizations.R
+++ R/Visualizations.R
@@ -80,8 +80,8 @@ bias2 <- bias_combined %>%
     geom_vline(xintercept = 0, color = "gray40") +
     geom_segment(y = 0, yend = 1.5, x = -2.5, linetype = "dashed", color = "gray40") +
     geom_segment(y = 0, yend = 1.5, x = 2.5, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 1.5, yend = 2.5, x = -8.474903, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 1.5, yend = 2.5, x = 8.474903, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 1.5, yend = 3, x = -8.474903, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 1.5, yend = 3, x = 8.474903, linetype = "dashed", color = "gray40") +
     scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 9, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
@@ -102,14 +102,14 @@ biasintercept <- bias_combined %>%
     geom_point(position = position_jitter(seed = 123, width = 0, height = .3)) +
     geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123, width = 0, height = .3), width = .08) +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
-    scale_x_continuous(n.breaks = 20) +
+    scale_x_continuous(n.breaks = 10, minor_breaks = seq(-.9, .7, .05)) +
     scale_y_discrete(limits = c("beta0j"), labels = c(expression(paste(gamma, "00")))) + 
     geom_vline(xintercept = 0, color = "gray40") +
-    geom_segment(y = 0, yend = 1.5, x = -.1, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 0, yend = 1.5, x = .1, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 2, x = -.1, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 2, x = .1, linetype = "dashed", color = "gray40") +
     scale_color_manual(values = cbbPalette) +
     theme_minimal() +
-    theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 7, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
+    theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 9, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
     labs(
         x = "Bias",
         y = "Term",
@@ -143,7 +143,7 @@ biaslevel1 <- bias_combined %>%
 ggsave(biaslevel1, file = "thesis/graphs/biaslevel1.png",  width = 23, height = 20, units = "cm", dpi = "retina", bg = "white")
 # Fixed level-2 effects
 biaslevel2 <- bias_combined %>%
-    filter(Term == "z1" | Term == "z2" | Term == "x1:z1" | Term == "x2:z1" | Term == "x3:z2") %>%
+    filter(Term == "z1" | Term == "z2") %>%
     ggplot(aes(
         x = Bias,
         y = Term,
@@ -152,22 +152,45 @@ biaslevel2 <- bias_combined %>%
     geom_point(position = position_jitter(seed = 123, width = 0, height = .3)) +
     geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123, width = 0, height = .3), width = .1) +
     facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
-    scale_x_continuous(n.breaks = 20) +
-    scale_y_discrete(limits = c("z1", "z2", "x1:z1", "x2:z1", "x3:z2"), labels = c(expression(paste(gamma, "01")), expression(paste(gamma, "02")), expression(paste(gamma, "11")), expression(paste(gamma, "21")), expression(paste(gamma, "32")))) + 
+    scale_x_continuous(n.breaks = 10, minor_breaks = seq(-.7, .9, .05)) +
+    scale_y_discrete(limits = c("z1", "z2"), labels = c(expression(paste(gamma, "01")), expression(paste(gamma, "02")))) + 
     geom_vline(xintercept = 0, color = "gray40") +
-    geom_segment(y = 0, yend = 2.5, x = -.05, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 0, yend = 2.5, x = .05, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 2.5, yend = 5.5, x = -.035, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 2.5, yend = 5.5, x = .035, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 3, x = -.05, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 3, x = .05, linetype = "dashed", color = "gray40") +
     scale_color_manual(values = cbbPalette) +
     theme_minimal() +
-    theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 7, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
+    theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 9, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
     labs(
         x = "Bias",
         y = "Term",
         color = "Method"
     )
-ggsave(biaslevel2, file = "thesis/graphs/biaslevel2.png",  width = 23, height = 23, units = "cm", dpi = "retina", bg = "white")
+ggsave(biaslevel2, file = "thesis/graphs/biaslevel2.png",  width = 23, height = 14, units = "cm", dpi = "retina", bg = "white")
+# Fixed cross-level effects
+biascrosslevel <- bias_combined %>%
+    filter(Term == "x1:z1" | Term == "x2:z1" | Term == "x3:z2") %>%
+    ggplot(aes(
+        x = Bias,
+        y = Term,
+        color = Method
+    )) +
+    geom_point(position = position_jitter(seed = 123, width = 0, height = .3)) +
+    geom_errorbar(aes(xmin = Bias - MCSE, xmax = Bias + MCSE), position = position_jitter(seed = 123, width = 0, height = .3), width = .15) +
+    facet_grid(cols = vars(`Number of groups`, `Group size`), rows = vars(`Missingness mechanism`), labeller = "label_both") +
+    scale_x_continuous(n.breaks = 10, minor_breaks = seq(-.325, .1, .025)) +
+    scale_y_discrete(limits = c("x1:z1", "x2:z1", "x3:z2"), labels = c(expression(paste(gamma, "11")), expression(paste(gamma, "21")), expression(paste(gamma, "32")))) + 
+    geom_vline(xintercept = 0, color = "gray40") +
+    geom_segment(y = 0, yend = 4, x = -.035, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 4, x = .035, linetype = "dashed", color = "gray40") +
+    scale_color_manual(values = cbbPalette) +
+    theme_minimal() +
+    theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 9, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
+    labs(
+        x = "Bias",
+        y = "Term",
+        color = "Method"
+    )
+ggsave(biascrosslevel, file = "thesis/graphs/biascrosslevel.png",  width = 23, height = 15, units = "cm", dpi = "retina", bg = "white")
 # Random slopes
 biasrandom <- bias_combined %>%
     filter(Term == "u1" | Term == "u2" | Term == "u3") %>%
@@ -182,8 +205,8 @@ biasrandom <- bias_combined %>%
     scale_x_continuous(n.breaks = 10, minor_breaks = seq(-.6, .25, .025)) +
     scale_y_discrete(limits = c("u1", "u2", "u3"), labels = c(expression(paste(upsilon, "1")), expression(paste(upsilon, "2")), expression(paste(upsilon, "3")))) + 
     geom_vline(xintercept = 0, color = "gray40") +
-    geom_segment(y = 0, yend = 3.5, x = -.1, linetype = "dashed", color = "gray40") +
-    geom_segment(y = 0, yend = 3.5, x = .1, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 4, x = -.1, linetype = "dashed", color = "gray40") +
+    geom_segment(y = 0, yend = 4, x = .1, linetype = "dashed", color = "gray40") +
     scale_color_manual(values = cbbPalette) +
     theme_minimal() +
     theme(panel.border = element_rect(colour = "gray25", fill = NA, size = .5), axis.text.x = element_text(size = 9, angle = 45, hjust = 1), axis.text.y = element_text(size = 9), legend.position = "bottom", legend.text = element_text(size = 9), axis.title = element_text(size = 12), legend.title = element_text(size = 12), panel.grid = element_line(color = "gray80")) +
