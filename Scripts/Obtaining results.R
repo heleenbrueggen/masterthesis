846246d5e737e0b4db224fc4abb84944bb5575a8
heleenbrueggen <heleen355@gmail.com>
2024-05-15 16:02:02 +0200

    Chaning paths to be relative

--- Scripts/Obtaining results.R
+++ Scripts/Obtaining results.R
@@ -12,48 +12,48 @@ set.seed(123)
 # Complete analysis # 
 #####################
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/Complete analyses.R", encoding = "UTF-8")
+source("scripts/analyses/Complete analyses.R", encoding = "UTF-8")
 # Results 
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results complete analyses.R", encoding = "UTF-8")
+source("scripts/results/Results complete analyses.R", encoding = "UTF-8")
 #####################
 # Listwise deletion #
 #####################
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/Listwise deletion analyses.R", encoding = "UTF-8")
+source("scripts/analyses/Listwise deletion analyses.R", encoding = "UTF-8")
 # Results
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results listwise deletion analyses.R", encoding = "UTF-8")
+source("scripts/results/Results listwise deletion analyses.R", encoding = "UTF-8")
 #######
 # PMM #
 #######
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/pmm analyses.R", encoding = "UTF-8")
+source("scripts/analyses/pmm analyses.R", encoding = "UTF-8")
 # Results
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results pmm analyses.R", encoding = "UTF-8")
+source("scripts/results/Results pmm analyses.R", encoding = "UTF-8")
 ##########
 # 2l.PMM #
 ##########
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/2l.pmm analyses.R", encoding = "UTF-8")
+source("scripts/analyses/2l.pmm analyses.R", encoding = "UTF-8")
 # Results
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results 2l.pmm analyses.R", encoding = "UTF-8")
+source("scripts/results/Results 2l.pmm analyses.R", encoding = "UTF-8")
 ########
 # bart #
 ########
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/bart analyses.R", encoding = "UTF-8")
+source("scripts/analyses/bart analyses.R", encoding = "UTF-8")
 # Results
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results bart analyses.R", encoding = "UTF-8")
+source("scripts/results/Results bart analyses.R", encoding = "UTF-8")
 #########
 # rbart #
 #########
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/rbart analyses.R", encoding = "UTF-8")
+source("scripts/analyses/rbart analyses.R", encoding = "UTF-8")
 # Results
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results rbart analyses.R", encoding = "UTF-8")
+source("scripts/results/Results rbart analyses.R", encoding = "UTF-8")
 #############
 # stan4bart #
 #############
 # Analyses
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/analyses thesis/stan4bart analyses.R", encoding = "UTF-8")
+source("scripts/analyses/stan4bart analyses.R", encoding = "UTF-8")
 # Results
-source("/Users/Heleen/Desktop/Universiteit Utrecht/Methodology & Statistics for the Behavioural, Biomedical and Social Sciences/Year 2/Master Thesis/masterthesis/R/results thesis/Results stan4bart analyses.R", encoding = "UTF-8")
+source("scripts/results/Results stan4bart analyses.R", encoding = "UTF-8")
