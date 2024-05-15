d3e793953363688c8e14d826aef43905fb5daa09
heleenbrueggen <heleen355@gmail.com>
2024-05-15 16:00:25 +0200

    Annotations and checking code

--- Scripts/results/Results complete analyses.R
+++ Scripts/results/Results complete analyses.R
@@ -13,26 +13,12 @@ set.seed(123)
 #######################
 # Defining parameters #
 #######################
-# ngroups <- c(30, 50)
-# groupsizes <- c(15, 35, 50)
-# iccs <- c(.2, .5)
-# mar_mcar <- c("mcar")
-# miss <- c(0)
-# g <- c(.2, .5)
-# combinations <- expand.grid(
-#   ngroup = ngroups,
-#   groupsize = groupsizes,
-#   icc = iccs,
-#   mar_mcar = mar_mcar,
-#   miss = miss,
-#   g = g
-# )
-ngroups <- c(30, 50)
-groupsizes <- c(15, 50)
-iccs <- c(.5)
-mar_mcar <- c("mar", "mcar")
-miss <- c(0)
-g <- c(.5)
+ngroups <- c(30, 50) # Number of groups 
+groupsizes <- c(15, 50) # Group sizes 
+iccs <- c(.5) # Intraclass correlation coefficient
+mar_mcar <- c("mar", "mcar") # Missing data mechanism
+miss <- c(0) # Percentage of missing data
+g <- c(.5) # Within-group effect size
 combinations <- expand.grid(
   ngroup = ngroups,
   groupsize = groupsizes,
@@ -61,13 +47,14 @@ for (i in seq_len(nrow(combinations))) {
 ###################
 results_nomiss <- list()
 for (i in seq_len(nrow(combinations))) {
-  results_nomiss[[i]] <- read_rds(paste("/Volumes/Heleen\ 480GB/Master\ thesis/results/nomissing/analyses_nomiss_", names[i], ".rds", sep = ""))[1:100]
+  results_nomiss[[i]] <- read_rds(paste("results/nomissing/analyses_nomiss_", names[i], ".rds", sep = ""))
 }
 #####################################
 # Creating functions for evaluation # 
 #####################################
 # Bias function
 bias <- function(estimated) {
+  # Extracting estimates
   estimates <- estimated %>%
     map(function(x) {
       fixed <- x %>%
@@ -79,7 +66,7 @@ bias <- function(estimated) {
         sapply(function(x) x^2)
       c(fixed, random)
     })
-
+  # Defining truth
   truth <- tibble(
     beta0j = 10,
     beta1j = .5,
@@ -100,19 +87,21 @@ bias <- function(estimated) {
     u3 = 1,
     eij = 25
   )
-
+  # Bias for all data sets
   bias.datasets <- map(estimates, \(x) (x - truth)) %>%
     list_rbind() %>%
     as_tibble()
+  # Average bias
   bias <- colMeans(bias.datasets) %>%
     t() %>%
     as_tibble()
-
+  # Mean of estimates
   mean_estimates <- estimates %>%
     do.call(rbind, .) %>%
     colMeans() %>%
     t() %>%
     as_tibble()
+  # Calculating MCSE
   mcse <- map(estimates, \(x) (x - mean_estimates)^2) %>%
     list_rbind() %>%
     colSums() %>%
@@ -123,65 +112,14 @@ bias <- function(estimated) {
 
   return(list(bias.datasets = bias.datasets, bias = bias, bias.mcse = mcse))
 }
-# MSE function
-mse <- function(estimated) {
-  estimates <- estimated %>%
-    map(function(x) {
-      fixed <- x %>%
-        filter(term == "(Intercept)" | term == "x1" | term == "x2" | term == "x3" | term == "x4" | term == "x5" | term == "x6" | term == "x7" | term == "z1" | term == "z2" | term == "x1:z1" | term == "x2:z1" | term == "x3:z2") %>%
-        pull(estimate, term)
-      random <- x %>%
-        filter(term == "sd__(Intercept)" | term == "sd__x1" | term == "sd__x2" | term == "sd__x3" | term == "sd__Observation") %>%
-        pull(estimate, term) %>%
-        sapply(function(x) x^2)
-      c(fixed, random)
-    })
-
-  truth <- tibble(
-    beta0j = 10,
-    beta1j = .5,
-    beta2j = .5,
-    beta3j = .5,
-    beta4j = .5,
-    beta5j = .5,
-    beta6j = .5,
-    beta7j = .5,
-    z1 = .5,
-    z2 = .5,
-    `x1:z1` = .35,
-    `x2:z1` = .35,
-    `x3:z2` = .35,
-    u0 = 84.74903,
-    u1 = 1,
-    u2 = 1,
-    u3 = 1,
-    eij = 25
-  )
-
-  mse.datasets <- map(estimates, \(x) (x - truth)^2) %>%
-    list_rbind() %>%
-    as_tibble()
-  mse <- colMeans(mse.datasets) %>%
-    t() %>%
-    as_tibble()
-
-  mcse <- map(estimates, \(x) (x - truth)^2) %>%
-    map(., \(x) (x - mse)^2) %>%
-    list_rbind() %>%
-    colSums() %>%
-    map_vec(\(x) sqrt(x / (length(estimates) * (length(estimates) - 1)))) %>% 
-    t() %>%
-    as_tibble()
-
-  return(list(mse.datasets = mse.datasets, mse = mse, mse.mcse = mcse))
-}
 # Coverage function
 coverage <- function(estimated) {
+  # Extracting estimates
   estimates <- estimated %>%
     map(~ .x %>%
       filter(term == "(Intercept)" | term == "x1" | term == "x2" | term == "x3" | term == "x4" | term == "x5" | term == "x6" | term == "x7" | term == "z1" | term == "z2" | term == "x1:z1" | term == "x2:z1" | term == "x3:z2") %>%
       select(conf.low, conf.high))
-
+  # Defining truth
   truth <- tibble(
     beta0j = 10,
     beta1j = .5,
@@ -200,9 +138,9 @@ coverage <- function(estimated) {
     t() %>% 
     as_tibble() %>% 
     rename(value = V1)
-
+  # Combining estimates and truth
   combined <- map(estimates, \(x) cbind(x, truth))
-
+  # Coverage of all data sets
   coverage.datasets <- map(combined, \(x) x %>%
     mutate(coverage = value > conf.low & value < conf.high) %>%
     select(coverage) %>%
@@ -211,36 +149,37 @@ coverage <- function(estimated) {
     as_tibble() %>%
     rename(beta0j = V1, beta1j = V2, beta2j = V3, beta3j = V4, beta4j = V5, beta5j = V6, beta6j = V7, beta7j = V8, z1 = V9, z2 = V10, `x1:z1` = V11, `x2:z1` = V12, `x3:z2` = V13)) %>%
     list_rbind()
-
+  # Average coverage
   coverage <- colMeans(coverage.datasets) %>%
     t() %>%
     as_tibble()
-
+  # MCSE of coverage
   mcse <- sqrt(coverage * (1 - coverage) / length(estimates)) %>%
     as_tibble()
 
   return(list(coverage.datasets = coverage.datasets, coverage = coverage, coverage.mcse = mcse))
 }
 # Confidence interval width function
-ciw <- function(estimated) { 
+ciw <- function(estimated) {
+  # Extracting estimates
   estimates <- estimated %>%
     map(~ .x %>%
       filter(term == "(Intercept)" | term == "x1" | term == "x2" | term == "x3" | term == "x4" | term == "x5" | term == "x6" | term == "x7" | term == "z1" | term == "z2" | term == "x1:z1" | term == "x2:z1" | term == "x3:z2") %>%
       select(conf.low, conf.high))
+  # Calculating CIW for all datasets
+  ciw.datasets <- map(estimates, ~ .x %>%
+    mutate(ciw = conf.high - conf.low) %>%
+    select(ciw) %>%
+    t() %>%
+    as_tibble() %>%
+    rename(beta0j = V1, beta1j = V2, beta2j = V3, beta3j = V4, beta4j = V5, beta5j = V6, beta6j = V7, beta7j = V8, z1 = V9, z2 = V10, `x1:z1` = V11, `x2:z1` = V12, `x3:z2` = V13)) %>%
+    list_rbind()
+  # Average CIW
+  ciw <- colMeans(ciw.datasets) %>%
+    t() %>%
+    as_tibble()
 
-    ciw.datasets <- map(estimates, ~ .x %>%
-      mutate(ciw = conf.high - conf.low) %>%
-      select(ciw) %>%
-      t() %>%
-      as_tibble() %>%
-      rename(beta0j = V1, beta1j = V2, beta2j = V3, beta3j = V4, beta4j = V5, beta5j = V6, beta6j = V7, beta7j = V8, z1 = V9, z2 = V10, `x1:z1` = V11, `x2:z1` = V12, `x3:z2` = V13)) %>% 
-      list_rbind()
-
-    ciw <- colMeans(ciw.datasets) %>%
-      t() %>%
-      as_tibble()
-    
-    return(list(ciw.datasets = ciw.datasets, ciw = ciw))
+  return(list(ciw.datasets = ciw.datasets, ciw = ciw))
 }
 ##############
 # Evaluation # 
@@ -253,12 +192,13 @@ for (i in seq_len(nrow(combinations))) {
     # Bias
     bias.datasets_nomiss[[i]] <- bias(results_nomiss[[i]])
 }
+# Extracting relevant information
 bias_nomiss <- map(bias.datasets_nomiss, ~ .x$bias) %>% list_rbind()
 mcse_bias_nomiss <- map(bias.datasets_nomiss, ~ .x$bias.mcse) %>% list_rbind()
 # Saving
-write_rds(bias.datasets_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/bias_datasets_nomiss.rds")
-write_rds(bias_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/bias_nomiss.rds")
-write_rds(mcse_bias_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mcse_bias_nomiss.rds")
+write_rds(bias.datasets_nomiss, file = "results/evaluations/bias_datasets_nomiss.rds")
+write_rds(bias_nomiss, file = "results/evaluations/bias_nomiss.rds")
+write_rds(mcse_bias_nomiss, file = "results/evaluations/mcse_bias_nomiss.rds")
 # MSE 
 mse.datasets_nomiss <- list()
 for (i in seq_len(nrow(combinations))) {
@@ -267,12 +207,13 @@ for (i in seq_len(nrow(combinations))) {
     # MSE
     mse.datasets_nomiss[[i]] <- mse(results_nomiss[[i]])
 }
+# Extracting relevant information
 mse_nomiss <- map(mse.datasets_nomiss, ~ .x$mse) %>% list_rbind()
 mcse_mse_nomiss <- map(mse.datasets_nomiss, ~ .x$mse.mcse) %>% list_rbind()
 # Saving
-write_rds(mse.datasets_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mse_datasets_nomiss.rds")
-write_rds(mse_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mse_nomiss.rds")
-write_rds(mcse_mse_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mcse_mse_nomiss.rds")
+write_rds(mse.datasets_nomiss, file = "results/evaluations/mse_datasets_nomiss.rds")
+write_rds(mse_nomiss, file = "results/evaluations/mse_nomiss.rds")
+write_rds(mcse_mse_nomiss, file = "results/evaluations/mcse_mse_nomiss.rds")
 # Coverage
 coverage.datasets_nomiss <- list()
 for (i in seq_len(nrow(combinations))) {
@@ -281,12 +222,13 @@ for (i in seq_len(nrow(combinations))) {
     # Coverage
     coverage.datasets_nomiss[[i]] <- coverage(results_nomiss[[i]])
 }
+# Extracting relevant information
 coverage_nomiss <- map(coverage.datasets_nomiss, ~ .x$coverage) %>% list_rbind()
 mcse_coverage_nomiss <- map(coverage.datasets_nomiss, ~ .x$coverage.mcse) %>% list_rbind()
 # Saving
-write_rds(coverage.datasets_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/coverage_datasets_nomiss.rds")
-write_rds(coverage_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/coverage_nomiss.rds")
-write_rds(mcse_coverage_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mcse_coverage_nomiss.rds")
+write_rds(coverage.datasets_nomiss, file = "results/evaluations/coverage_datasets_nomiss.rds")
+write_rds(coverage_nomiss, file = "results/evaluations/coverage_nomiss.rds")
+write_rds(mcse_coverage_nomiss, file = "results/evaluations/mcse_coverage_nomiss.rds")
 # CIW
 ciw.datasets_nomiss <- list()
 for (i in seq_len(nrow(combinations))) {
@@ -295,7 +237,8 @@ for (i in seq_len(nrow(combinations))) {
     # CIW
     ciw.datasets_nomiss[[i]] <- ciw(results_nomiss[[i]])
 }
+# Extracting relevant information
 ciw_nomiss <- map(ciw.datasets_nomiss, ~ .x$ciw) %>% list_rbind()
 # Saving
-write_rds(ciw.datasets_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/ciw_datasets_nomiss.rds")
-write_rds(ciw_nomiss, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/ciw_nomiss.rds")
+write_rds(ciw.datasets_nomiss, file = "results/evaluations/ciw_datasets_nomiss.rds")
+write_rds(ciw_nomiss, file = "results/evaluations/ciw_nomiss.rds")
--- Scripts/results/Results stan4bart analyses.R
+++ Scripts/results/Results stan4bart analyses.R
@@ -14,26 +14,12 @@ set.seed(123)
 #######################
 # Defining parameters #
 #######################
-# ngroups <- c(30, 50)
-# groupsizes <- c(15, 35, 50)
-# iccs <- c(.2, .5)
-# mar_mcar <- c("mar", "mcar")
-# miss <- c(25, 50)
-# g <- c(.2, .5)
-# combinations <- expand.grid(
-#   ngroup = ngroups,
-#   groupsize = groupsizes,
-#   icc = iccs,
-#   mar_mcar = mar_mcar,
-#   miss = miss,
-#   g = g
-# )
-ngroups <- c(30, 50)
-groupsizes <- c(15, 50)
-iccs <- c(.5)
-mar_mcar <- c("mar", "mcar")
-miss <- c(50)
-g <- c(.5)
+ngroups <- c(30, 50) # Number of groups 
+groupsizes <- c(15, 50) # Group sizes 
+iccs <- c(.5) # Intraclass correlation coefficient
+mar_mcar <- c("mar", "mcar") # Missing data mechanism
+miss <- c(50) # Percentage of missing data
+g <- c(.5) # Within-group effect size
 combinations <- expand.grid(
   ngroup = ngroups,
   groupsize = groupsizes,
@@ -62,19 +48,20 @@ for (i in seq_len(nrow(combinations))) {
 ###################
 results_stan4bart <- list()
 for (i in seq_len(nrow(combinations))) {
-  results_stan4bart[[i]] <- read_rds(paste("/Volumes/Heleen\ 480GB/Master\ thesis/results/imputed/stan4bart/analyses_stan4bart_", names[i], ".rds", sep = ""))
+  results_stan4bart[[i]] <- read_rds(paste("results/imputed/stan4bart/analyses_stan4bart_", names[i], ".rds", sep = ""))
 }
 #####################################
 # Creating functions for evaluation # 
 #####################################
 # Bias function
 bias <- function(estimated) {
+  # Extracting estimates
   estimates <- estimated %>%
     map(., \(x) c(
       x[2]$estimates[1:13, 1],
       x[3]$extra.pars[c(1, 2, 3, 4, 11), ]
     ))
-
+  # Defininf truth
   truth <- tibble(
     beta0j = 10,
     beta1j = .5,
@@ -95,19 +82,21 @@ bias <- function(estimated) {
     u3 = 1,
     eij = 25
   )
-
+  # Calculating bias for all datasets
   bias.datasets <- map(estimates, \(x) (x - truth)) %>%
     list_rbind() %>%
     as_tibble()
+  # Average bias
   bias <- colMeans(bias.datasets) %>%
     t() %>%
     as_tibble()
-
+  # Mean of estimates
   mean_estimates <- estimates %>%
     do.call(rbind, .) %>%
     colMeans() %>%
     t() %>%
     as_tibble()
+  # MCSE of bias
   mcse <- map(estimates, \(x) (x - mean_estimates)^2) %>%
     list_rbind() %>%
     colSums() %>%
@@ -118,57 +107,12 @@ bias <- function(estimated) {
 
   return(list(bias.datasets = bias.datasets, bias = bias, bias.mcse = mcse))
 }
-# MSE function
-mse <- function(estimated) {
-  estimates <- estimated %>%
-    map(., \(x) c(
-      x[2]$estimates[1:13, 1],
-      x[3]$extra.pars[c(1, 2, 3, 4, 11), ]
-    ))
-
-  truth <- tibble(
-    beta0j = 10,
-    beta1j = .5,
-    beta2j = .5,
-    beta3j = .5,
-    beta4j = .5,
-    beta5j = .5,
-    beta6j = .5,
-    beta7j = .5,
-    z1 = .5,
-    z2 = .5,
-    `x1:z1` = .35,
-    `x2:z1` = .35,
-    `x3:z2` = .35,
-    u0 = 84.74903,
-    u1 = 1,
-    u2 = 1,
-    u3 = 1,
-    eij = 25
-  )
-
-  mse.datasets <- map(estimates, \(x) (x - truth)^2) %>%
-    list_rbind() %>%
-    as_tibble()
-  mse <- colMeans(mse.datasets) %>%
-    t() %>%
-    as_tibble()
-
-  mcse <- map(estimates, \(x) (x - truth)^2) %>%
-    map(., \(x) (x - mse)^2) %>%
-    list_rbind() %>%
-    colSums() %>%
-    map_vec(\(x) sqrt(x / (length(estimates) * (length(estimates) - 1)))) %>%
-    t() %>%
-    as_tibble()
-
-  return(list(mse.datasets = mse.datasets, mse = mse, mse.mcse = mcse))
-}
 # Coverage function
 coverage <- function(estimated) {
+  # Extracting estimates
   estimates <- estimated %>%
     map(~ .x %>% mitml::confint.mitml.testEstimates())
-
+  # Defining truth
   truth <- tibble(
     beta0j = 10,
     beta1j = .5,
@@ -187,9 +131,9 @@ coverage <- function(estimated) {
     t() %>% 
     as_tibble() %>% 
     rename(value = V1)
-
+  # Combining estimates and truth
   combined <- map(estimates, \(x) cbind(x, truth))
-
+  # Coverage of all data sets
   coverage.datasets <- map(combined, ~ .x %>%
     mutate(coverage = value > `2.5 %` & value < `97.5 %`) %>%
     select(coverage) %>%
@@ -198,35 +142,37 @@ coverage <- function(estimated) {
     as_tibble() %>%
     rename(beta0j = `(Intercept)`, beta1j = x1, beta2j = x2, beta3j = x3, beta4j = x4, beta5j = x5, beta6j = x6, beta7j = x7, z1 = z1, z2 = z2, `x1:z1` = `x1:z1`, `x2:z1` = `x2:z1`, `x3:z2` = `x3:z2`)) %>%
     list_rbind()
-
+  # Average coverage
   coverage <- colMeans(coverage.datasets) %>%
     t() %>%
     as_tibble()
-
+  # MCSE of coverage
   mcse <- sqrt(coverage * (1 - coverage) / length(estimates)) %>%
     as_tibble()
 
   return(list(coverage.datasets = coverage.datasets, coverage = coverage, coverage.mcse = mcse))
 }
 # Confidence interval width function
-ciw <- function(estimated) { 
+ciw <- function(estimated) {
+  # Extracting estimates
   estimates <- estimated %>%
-    map(~ .x %>% mitml::confint.mitml.testEstimates() %>% 
-    as_tibble())
+    map(~ .x %>%
+      mitml::confint.mitml.testEstimates() %>%
+      as_tibble())
+  # Calculating CIW for all datasets
+  ciw.datasets <- map(estimates, ~ .x %>%
+    mutate(ciw = `97.5 %` - `2.5 %`) %>%
+    select(ciw) %>%
+    t() %>%
+    as_tibble() %>%
+    rename(beta0j = V1, beta1j = V2, beta2j = V3, beta3j = V4, beta4j = V5, beta5j = V6, beta6j = V7, beta7j = V8, z1 = V9, z2 = V10, `x1:z1` = V11, `x2:z1` = V12, `x3:z2` = V13)) %>%
+    list_rbind()
+  # Average CIW
+  ciw <- colMeans(ciw.datasets) %>%
+    t() %>%
+    as_tibble()
 
-    ciw.datasets <- map(estimates, ~ .x %>%
-      mutate(ciw = `97.5 %` - `2.5 %`) %>%
-      select(ciw) %>%
-      t() %>%
-      as_tibble() %>%
-      rename(beta0j = V1, beta1j = V2, beta2j = V3, beta3j = V4, beta4j = V5, beta5j = V6, beta6j = V7, beta7j = V8, z1 = V9, z2 = V10, `x1:z1` = V11, `x2:z1` = V12, `x3:z2` = V13)) %>% 
-      list_rbind()
-
-    ciw <- colMeans(ciw.datasets) %>%
-      t() %>%
-      as_tibble()
-    
-    return(list(ciw.datasets = ciw.datasets, ciw = ciw))
+  return(list(ciw.datasets = ciw.datasets, ciw = ciw))
 }
 ##############
 # Evaluation # 
@@ -239,12 +185,13 @@ for (i in seq_len(nrow(combinations))) {
     # Bias
     bias.datasets_stan4bart[[i]] <- bias(results_stan4bart[[i]])
 }
+# Extracting relevant information
 bias_stan4bart <- map(bias.datasets_stan4bart, ~ .x$bias) %>% list_rbind()
 mcse_bias_stan4bart <- map(bias.datasets_stan4bart, ~ .x$bias.mcse) %>% list_rbind()
 # Saving
-write_rds(bias.datasets_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/bias_datasets_stan4bart.rds")
-write_rds(bias_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/bias_stan4bart.rds")
-write_rds(mcse_bias_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mcse_bias_stan4bart.rds")
+write_rds(bias.datasets_stan4bart, file = "results/evaluations/bias_datasets_stan4bart.rds")
+write_rds(bias_stan4bart, file = "results/evaluations/bias_stan4bart.rds")
+write_rds(mcse_bias_stan4bart, file = "results/evaluations/mcse_bias_stan4bart.rds")
 # MSE 
 mse.datasets_stan4bart <- list()
 for (i in seq_len(nrow(combinations))) {
@@ -253,12 +200,13 @@ for (i in seq_len(nrow(combinations))) {
     # MSE
     mse.datasets_stan4bart[[i]] <- mse(results_stan4bart[[i]])
 }
+# Extracting relevant information
 mse_stan4bart <- map(mse.datasets_stan4bart, ~ .x$mse) %>% list_rbind()
 mcse_mse_stan4bart <- map(mse.datasets_stan4bart, ~ .x$mse.mcse) %>% list_rbind()
 # Saving
-write_rds(mse.datasets_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mse_datasets_stan4bart.rds")
-write_rds(mse_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mse_stan4bart.rds") 
-write_rds(mcse_mse_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mcse_mse_stan4bart.rds")
+write_rds(mse.datasets_stan4bart, file = "results/evaluations/mse_datasets_stan4bart.rds")
+write_rds(mse_stan4bart, file = "results/evaluations/mse_stan4bart.rds") 
+write_rds(mcse_mse_stan4bart, file = "results/evaluations/mcse_mse_stan4bart.rds")
 # Coverage
 coverage.datasets_stan4bart <- list()
 for (i in seq_len(nrow(combinations))) {
@@ -267,12 +215,13 @@ for (i in seq_len(nrow(combinations))) {
     # Coverage
     coverage.datasets_stan4bart[[i]] <- coverage(results_stan4bart[[i]])
 }
+# Extracting relevant information
 coverage_stan4bart <- map(coverage.datasets_stan4bart, ~ .x$coverage) %>% list_rbind()
 mcse_coverage_stan4bart <- map(coverage.datasets_stan4bart, ~ .x$coverage.mcse) %>% list_rbind()
 # Saving
-write_rds(coverage.datasets_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/coverage_datasets_stan4bart.rds")
-write_rds(coverage_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/coverage_stan4bart.rds")
-write_rds(mcse_coverage_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/mcse_coverage_stan4bart.rds")
+write_rds(coverage.datasets_stan4bart, file = "results/evaluations/coverage_datasets_stan4bart.rds")
+write_rds(coverage_stan4bart, file = "results/evaluations/coverage_stan4bart.rds")
+write_rds(mcse_coverage_stan4bart, file = "results/evaluations/mcse_coverage_stan4bart.rds")
 # CIW
 ciw.datasets_stan4bart <- list()
 for (i in seq_len(nrow(combinations))) {
@@ -281,7 +230,8 @@ for (i in seq_len(nrow(combinations))) {
     # CIW
     ciw.datasets_stan4bart[[i]] <- ciw(results_stan4bart[[i]])
 }
+# Extracting relevant information
 ciw_stan4bart <- map(ciw.datasets_stan4bart, ~ .x$ciw) %>% list_rbind()
 # Saving
-write_rds(ciw.datasets_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/ciw_datasets_stan4bart.rds")
-write_rds(ciw_stan4bart, file = "/Volumes/Heleen\ 480GB/Master\ thesis/results/evaluations/ciw_stan4bart.rds")
+write_rds(ciw.datasets_stan4bart, file = "results/evaluations/ciw_datasets_stan4bart.rds")
+write_rds(ciw_stan4bart, file = "results/evaluations/ciw_stan4bart.rds")
